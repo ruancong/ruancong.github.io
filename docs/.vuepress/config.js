1facf6dd2a8418b6b2ef3b6c2889d2ecbd944672
RuanCong <1308811723@qq.com>
2024-10-10 15:18:56 +0800

    feature: 修改目录等级数

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,21 +1,22 @@
-import { defaultTheme } from '@vuepress/theme-default'
-import { defineUserConfig } from 'vuepress/cli'
-import { viteBundler } from '@vuepress/bundler-vite'
+import {defaultTheme} from '@vuepress/theme-default'
+import {defineUserConfig} from 'vuepress/cli'
+import {viteBundler} from '@vuepress/bundler-vite'
 
 export default defineUserConfig({
-  lang: 'zh-CN',
+    lang: 'zh-CN',
 
-  title: '笔记',
-  description: '学习笔记',
+    title: '笔记',
+    description: '学习笔记',
 
-  theme: defaultTheme({
-    //logo: 'https://vuejs.press/images/hero.png',
+    theme: defaultTheme({
+        //logo: 'https://vuejs.press/images/hero.png',
 
-    navbar: [{
-        text: '首页',
-        link: '/',
-      }],
-  }),
+        navbar: [{
+            text: '首页',
+            link: '/',
+        }],
+        sidebarDepth: 3,
+    }),
 
-  bundler: viteBundler(),
+    bundler: viteBundler(),
 })
